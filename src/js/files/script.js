d96238d0a519800d5793f99c3ebbe699b55a20e4
Jennie Dry <evg.dry@gmail.com>
2023-08-18 16:24:07 +0300

    Swiper

---
+++ src/js/files/script.js
@@ -0,0 +1,9 @@
+// Подключение функционала "Чертогов Фрилансера"
+//import { listeners } from "gulp";
+//import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+//import { flsModules } from "./modules.js";
+
+window.addEventListener("load", function(e) {
+   document.body.insertAdjacentHTML('beforeend','<div class="main-bg"></div>');
+});
